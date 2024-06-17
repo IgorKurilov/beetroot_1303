845f93aaf5a0a35284f5d3f56dbb6b48e5989c70
IgorKurilov <45769419+IgorKurilov@users.noreply.github.com>
2024-06-17 16:31:19 +0300

    Create task1.py

---
+++ lesson36/task1.py
@@ -0,0 +1,24 @@
+import threading
+
+# Global variables
+counter = 0
+rounds = 100000
+
+class Counter(threading.Thread):
+    def run(self):
+        global counter
+        for _ in range(rounds):
+            counter += 1
+
+# Create and start threads
+thread1 = Counter()
+thread2 = Counter()
+
+thread1.start()
+thread2.start()
+
+thread1.join()
+thread2.join()
+
+# Check the result of the counter
+print("Final counter value:", counter)
