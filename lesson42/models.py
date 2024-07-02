7116cba14dd88c00fec1d298124868abdac62394
IgorKurilov <45769419+IgorKurilov@users.noreply.github.com>
2024-07-02 09:23:23 +0300

    Create models.py

---
+++ lesson42/models.py
@@ -0,0 +1,17 @@
+# models.py
+from django.db import models
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    likes = models.IntegerField(default=0)
+    dislikes = models.IntegerField(default=0)
+
+    @property
+    def rating(self):
+        return self.likes - self.dislikes
+
+    def __str__(self):
+        return self.title
